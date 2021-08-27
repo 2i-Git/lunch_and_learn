24c04b24802a918c86b2d6d2d446757f1c4d996a
Katarzyna Kmiotek <katarzyna.kmiotek@Andrews-MacBook-Air.local>
2021-08-27 15:54:20 +0100

    logo

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,7 +26,7 @@ module.exports = {
         background_color: "#ffffff",
         theme_color: "#fed136",
         display: "minimal-ui",
-        icon: "content/assets/gatsby-icon.png",
+        icon: "content/assets/logo.png",
       },
     },
     "gatsby-transformer-remark",
